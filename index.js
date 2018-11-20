934239105ca4858cab196e800f8e060c6baf64d4
samaorahno <samabos007@gmail.com>
2018-11-20 17:14:32 +0100

    login page

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'public','/index.html')))
+// include the path to static files
+app.use(express.static(path.join(__dirname, 'public', 'static')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','signup.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','login.html'))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
